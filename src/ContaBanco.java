d3fed3594dd124e58dcd17520aef4f87d40aec93
igoralves141098 <igoralves141098@gmail.com>
2026-09-17 15:02:37 -0300

    Atividade finalizada

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -11,16 +11,57 @@ public class ContaBanco {
         saldo = 0.0;
         status = false;
         totalContasCriadas++;
+        this.numeroConta = totalContasCriadas;
     }
 
     public void abrirConta(TipoConta tipo){
+        if(this.status == true) {
+            throw new IllegalArgumentException("Erro: está conta já está aberta!");
+        }
+        this.tipoConta = tipo;
+
         if(tipo == TipoConta.CORRENTE) {
             this.saldo = 50.0;
-        }
-        else if(tipo == TipoConta.POUPANCA) {
+        } else if(tipo == TipoConta.POUPANCA) {
             this.saldo = 150.0;
         }
-        status = true;
+        this.status = true;
+    }
+
+    public void fecharConta() {
+        if(this.saldo > 0 || this.saldo < 0) {
+            throw new IllegalArgumentException("A conta possui saldo ou está em debito e não pode ser fechada!");
+        }
+        this.status = false;
+    }
+
+    public void depositar(double valor) {
+        if(this.status == false) {
+            throw new IllegalArgumentException("A conta está fechada");
+        } else if(valor <= 0) {
+            throw new IllegalArgumentException("Valor de deposito invalido");
+        }
+        this.saldo+=valor;
+    }
+
+    public void sacar(double valor) {
+        if(this.status == false) {
+            throw new IllegalArgumentException("A conta está fechada");
+        } else if(valor > this.saldo) {
+            throw new IllegalArgumentException("Saldo insuficiente para saque");
+        }
+        this.saldo = this.saldo - valor;
+    }
+
+    public void pagarMensalidade() {
+        if(this.status == false) {
+            throw new IllegalArgumentException("A conta está fechada, não é possivél a cobrança!");
+        }
+        if(this.tipoConta == TipoConta.CORRENTE) {
+            this.saldo = this.saldo - 12.0;
+        } else if(this.tipoConta == TipoConta.POUPANCA) {
+            this.saldo = this.saldo - 20.0;
+        }
     }
 
     public static int getTotalContasCriadas() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("==== ABERTURA DE CONTA=====");
+
+        try {
+            ContaBanco conta1 = new ContaBanco("Igor");
+
+            conta1.abrirConta(TipoConta.CORRENTE);
+            conta1.depositar(100);
+            conta1.pagarMensalidade();
+            conta1.sacar(15.75);
+            conta1.fecharConta();
+
+            ContaBanco conta2 = new ContaBanco("Pedro");
+
+            conta2.abrirConta(TipoConta.POUPANCA);
+            conta2.depositar(150);
+            conta2.pagarMensalidade();
+            conta2.sacar(35);
+            conta2.fecharConta();
+
+
+
+        } catch (Exception erro) {
+            System.out.println("Algo deu errado na abertura de conta" + erro.getMessage());
+        }
 
     }
 }
